590405906353e86d6bbb1572e7bb83d42484de1b
Work-Nantapong <work.h.nantapong@gmail.com>
2023-10-23 13:36:57 +0700

    redis cache

--- api/app/Http/Controllers/MapController.php
+++ api/app/Http/Controllers/MapController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 class MapController extends Controller
@@ -13,11 +14,18 @@ class MapController extends Controller
      */
     public function searchMap(Request $request): string
     {
-        $keyword = $request->input('keyword');
-        $url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants%20in%20".$keyword."&key=".env('GOOGLE_API_KEY');
-        $response = Http::get($url);
+        $response = Cache::get($request->input('keyword'));
 
-        return $response->body();
+        if (is_null($response)) {
+            $keyword = $request->input('keyword');
+            $url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants%20in%20" . $keyword . "&key=" . env('GOOGLE_API_KEY');
+            $response = Http::get($url);
+            $response = $response->body();
+
+            Cache::store('redis')->put($request->input('keyword'), $response, now()->addHours(1)); // 60 Minutes
+        }
+
+        return $response;
     }
 
     /**
@@ -26,9 +34,15 @@ class MapController extends Controller
      */
     public function getByPlaceID(string $PlaceID): string
     {
-        $url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=".$PlaceID."&key=".env('GOOGLE_API_KEY');
-        $response = Http::get($url);
+        $response = Cache::get($PlaceID);
 
-        return $response->body();
+        if (is_null($response)) {
+            $url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=".$PlaceID."&key=".env('GOOGLE_API_KEY');
+            $response = Http::get($url);
+            $response = $response->body();
+            Cache::store('redis')->put($PlaceID, $response, now()->addHours(1)); // 60 Minutes
+        }
+
+        return $response;
     }
 }
