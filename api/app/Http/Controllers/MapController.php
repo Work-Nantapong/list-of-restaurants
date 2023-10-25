399a72717b3be1fe77b49011c04ec5b03ab4a3a9
Work-Nantapong <work.h.nantapong@gmail.com>
2023-10-25 23:34:14 +0700

    change route api

--- api/app/Http/Controllers/MapController.php
+++ api/app/Http/Controllers/MapController.php
@@ -12,17 +12,16 @@ class MapController extends Controller
      * @param Request $request
      * @return string
      */
-    public function searchMap(Request $request): string
+    public function searchMap(string $keyword): string
     {
-        $response = Cache::get($request->input('keyword'));
+        $response = Cache::get($keyword);
 
         if (is_null($response)) {
-            $keyword = $request->input('keyword');
             $url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants%20in%20" . $keyword . "&key=" . env('GOOGLE_API_KEY');
             $response = Http::get($url);
             $response = $response->body();
 
-            Cache::store('redis')->put($request->input('keyword'), $response, now()->addHours(1)); // 60 Minutes
+            Cache::store('redis')->put($keyword, $response, now()->addHours(1)); // 60 Minutes
         }
 
         return $response;
