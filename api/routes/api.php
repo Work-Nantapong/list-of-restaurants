203c26a77f0a25ed92741ee6044d9f2e282777c8
Work-Nantapong <work.h.nantapong@gmail.com>
2023-10-27 01:00:10 +0700

    Auth and docker file

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\MapController;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +22,25 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/restaurant/nearby_search/{keyword}', [MapController::class, 'searchMap']);
-Route::get('map_by_place_id/{place_id}', [MapController::class, 'getByPlaceID']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/restaurant/nearby_search/{keyword}', [MapController::class, 'searchMap']);
+    Route::get('map_by_place_id/{place_id}', [MapController::class, 'getByPlaceID']);
+});
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+});
